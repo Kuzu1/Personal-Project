68367dbe0306453909227eb8f9d6307a57f09fdb
Kuzu1 <zachcotton168@gmail.com>
2024-08-27 15:10:07 +0700

    new files

--- index.php
+++ index.php
@@ -11,54 +11,9 @@
     <title>Main Page</title>
 </head>
 <body>
-   <div class="sidebar">
-       <!-- Sidebar Content -->
-       <div class="logo"></div>
-       <ul class="menu">
-           <li class="active">
-               <a href="#">
-                   <i class="ri-dashboard-3-fill"></i>
-                   <span>Dashboard</span>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <i class="ri-graduation-cap-line"></i>
-                   <span>Class</span>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <i class="ri-account-box-fill"></i>
-                   <span>Student</span>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <i class="ri-clapperboard-line"></i>
-                   <span>Google Classroom</span>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <i class="ri-school-fill"></i>
-                   <span>MyParagon</span>
-               </a>
-           </li>
-           <li>
-               <a href="#">
-                   <i class="ri-settings-2-line"></i>
-                   <span>Settings</span>
-               </a>
-           </li>
-           <li class="logout">
-               <a href="#">
-                   <i class="ri-logout-box-line"></i>
-                   <span>Logout</span>
-               </a>
-           </li>
-       </ul>
-   </div>
+    
+   <?php include 'sidebar.php'; ?>
+
    <div class="main--content">
        <!-- Header -->
        <div class="header--wrapper">
